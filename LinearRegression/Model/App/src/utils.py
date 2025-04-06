e8fbb6f34ae81714ce5689c6630e7fb2e6e778dc
Mohamed Ibrahim Moselhy <Mohamed_Moselhy@outlook.com>
2025-04-06 03:22:58 +0200

    path updated

--- LinearRegression/Model/App/src/utils.py
+++ LinearRegression/Model/App/src/utils.py
@@ -2,8 +2,8 @@ import numpy as np
 import joblib
 
 def predict(user_input):
-    LinearRegMod = joblib.load("D:\My drive\Course\Data Science\Models Testing\Data-Science-Projects\LinearRegression\Model\model.pkl")  # Load the trained model
-    MyScaler = joblib.load("D:\My drive\Course\Data Science\Models Testing\Data-Science-Projects\LinearRegression\Model\scaler.pkl")  # Load the saved scaler
+    LinearRegMod = joblib.load("model.pkl")  # Load the trained model
+    MyScaler = joblib.load("scaler.pkl")  # Load the saved scaler
     # Convert dict to 2D array if needed
     input_array = np.array([list(user_input.values())])
     input_array=MyScaler.transform(input_array)
